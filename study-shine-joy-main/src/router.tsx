9e9928f7bdda810e41cd52aaa8ff763a7d8cc156
Ramesh-Bonthu <rameshbonthu77@gmail.com>
2026-05-20 11:35:18 +0530

    Switch deployment to GitHub Pages

--- study-shine-joy-main/src/router.tsx
+++ study-shine-joy-main/src/router.tsx
@@ -58,6 +58,7 @@ export const getRouter = () => {
   const router = createRouter({
     routeTree,
     context: {},
+    basepath: '/LMS-Full-Stack-Project',
     scrollRestoration: true,
     defaultPreloadStaleTime: 0,
     defaultErrorComponent: DefaultErrorComponent,
